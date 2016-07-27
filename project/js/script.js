6f325ca1f459e480a938e28b38e66b1420d4a880
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-07-27 18:36:27 +0300

    add corrects

--- project/js/script.js
+++ project/js/script.js
@@ -2,24 +2,18 @@
 $(document).ready(function(){
 
 
-    $('.btn').click(function(){
-        $(this).prop('disabled', true);
+    $('.btn').on('click', function(event){
+        var targetElem = $(event.target);
+        var targetProduct = targetElem.closest('.product-tile');
+        $(targetElem).prop('disabled', true);
+        offProduct(targetProduct);
     });
 
 
     function offProduct(targetField){
         targetField.addClass('to-cart');
-        targetField.html('<p>Added to bag</p>');
+        targetField.append('<p>Added to bag</p>');
     }
 
-    var productList = document.getElementsByClassName('product-list');
-
-    productList.onclick = function (event){
-        var targetElem = event.target;
-        var targetProduct = target.closest('product-tile');
-            if(targetElem.is('button')){
-                offProduct(targetProduct);
-            }
-    }
 
 });
